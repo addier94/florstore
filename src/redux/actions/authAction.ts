1ebc17c4a7a628519f360418d59a94c817a68d02
Alfredo Fernandez Carlos <alfredofernandezcarlos@gmail.com>
2021-10-12 22:16:14 -0400

    start firebase config

--- src/redux/actions/authAction.ts
+++ src/redux/actions/authAction.ts
@@ -1,9 +1,13 @@
-import { createUserWithEmailAndPassword, getAuth } from "@firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  updateProfile,
+} from "@firebase/auth";
 import { Dispatch } from "redux";
 import { IUserRegister } from "../../utils/TypeScript";
 import { validRegister } from "../../utils/Valid";
 import { ALERT, IAlertType } from "../types/alertType";
-import { IAuthType } from "../types/authType";
+import { AUTH, IAuthType } from "../types/authType";
 
 export const startRegisterWithEmailPasswordName =
   (userRegister: IUserRegister) =>
@@ -15,7 +19,24 @@ export const startRegisterWithEmailPasswordName =
     }
 
     try {
+      dispatch({ type: ALERT, payload: { loading: true } });
+      const auth = getAuth();
+      const { name, email, password } = userRegister;
+      const { user } = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      await updateProfile(user, { displayName: name });
+
+      // if (user.displayName) {
+      dispatch({
+        type: AUTH,
+        payload: { uid: user.uid, name: user.displayName },
+      });
+      // }
     } catch (error: any) {
+      console.log("error", error);
       dispatch({ type: ALERT, payload: { errors: "Something wrong" } });
     }
   };
--- src/routers/AppRouter.tsx
+++ src/routers/AppRouter.tsx
@@ -1,10 +1,13 @@
+import { getAuth } from "@firebase/auth";
 import { BrowserRouter as Router, Switch, Redirect } from "react-router-dom";
+import app from "../services/firebase-config";
 import { AuthenticatedRoute } from "./AuthenticatedRoute";
 import { AuthRouter } from "./AuthRouter";
 import { PrivateRoute } from "./PrivateRoute";
 import { PublicRoute } from "./PublicRoute";
 
 export const AppRouter = () => {
+  const auth = getAuth(app);
   return (
     <Router>
       <Switch>
